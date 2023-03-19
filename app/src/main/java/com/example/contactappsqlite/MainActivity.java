e12faa6370a08c300b7d92c0fe8bcd39db89f458
Darron Tacio Moraes <111967762+darronmoraes@users.noreply.github.com>
2023-03-19 23:18:33 +0530

    added CRUD functionality and minor changes made to themes and other styling files

--- app/src/main/java/com/example/contactappsqlite/MainActivity.java
+++ app/src/main/java/com/example/contactappsqlite/MainActivity.java
@@ -1,14 +1,177 @@
 package com.example.contactappsqlite;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.contactappsqlite.adapter.ContactsAdapter;
+import com.example.contactappsqlite.db.DatabaseHelper;
+import com.example.contactappsqlite.db.entity.Contact;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Variables
+    private ContactsAdapter contactsAdapter;
+    private ArrayList<Contact> contactArrayList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private DatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Contact Manager");
+
+        // RecyclerView
+        recyclerView = findViewById(R.id.recycler_view_contacts);
+        db = new DatabaseHelper(this);
+
+        // Contacts List
+        contactArrayList.addAll(db.getAllContacts());
+        contactsAdapter = new ContactsAdapter(this, contactArrayList, MainActivity.this);
+
+        // Setting RecyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(contactsAdapter);
+
+        // Floating Action Button
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addAndEditContacts(false, null, -1);
+            }
+        });
+    }
+
+    /**
+     * Custom Methods
+     *
+     * */
+    public void addAndEditContacts(final boolean isUpdated, final Contact contact, final int position) {
+        LayoutInflater layoutInflater = LayoutInflater.from(getApplicationContext());
+        View view = layoutInflater.inflate(R.layout.layout_add_contact, null);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        alertDialogBuilder.setView(view);
+
+        TextView contactTitle = view.findViewById(R.id.tv_new_contact_title);
+        final EditText contactName = view.findViewById(R.id.et_name);
+        final EditText contactEmail = view.findViewById(R.id.et_email);
+
+        contactTitle.setText(!isUpdated ? "Add New Contact" : "Edit Contact");
+
+        if (isUpdated && contact != null) {
+            contactName.setText(contact.getName());
+            contactEmail.setText(contact.getEmail());
+        }
+
+        alertDialogBuilder.setCancelable(false)
+                .setPositiveButton(isUpdated ? "Update" : "Save", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setNegativeButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (isUpdated) {
+                            deleteContact(contact, position);
+                        } else {
+                            dialog.cancel();
+                        }
+                    }
+                });
+
+        final AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(contactName.getText().toString())) {
+                    Toast.makeText(MainActivity.this, "Please Enter Name", Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    alertDialog.dismiss();
+                }
+
+                if (isUpdated && contact != null) {
+                    updateContact(contactName.getText().toString(), contactEmail.getText().toString(), position);
+                } else {
+                    createContact(contactName.getText().toString(), contactEmail.getText().toString());
+                }
+            }
+        });
+    }
+
+    private void deleteContact(Contact contact, int position) {
+        contactArrayList.remove(position);
+        db.deleteContact(contact);
+        contactsAdapter.notifyDataSetChanged();
+    }
+
+    private void updateContact(String name, String email, int position) {
+        Contact contact = contactArrayList.get(position);
+
+        contact.setName(name);
+        contact.setEmail(email);
+
+        db.updateContact(contact);
+
+        contactArrayList.set(position, contact);
+        contactsAdapter.notifyDataSetChanged();
+    }
+
+    private void createContact(String name, String email) {
+        long id = db.insertContact(name, email);
+        Contact contact = db.getContact(id);
+
+        if (contact != null) {
+            contactArrayList.add(0, contact);
+            contactsAdapter.notifyDataSetChanged();
+        }
+    }
+
+    // MenuBar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
