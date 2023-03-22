4f4818327105db9e905d41768f3d6419e24e8180
Darron Tacio Moraes <111967762+darronmoraes@users.noreply.github.com>
2023-03-22 22:39:22 +0530

    modified entity contact class #ROOM annotations added replacing previous code

--- app/src/main/java/com/example/contactappsqlite/db/entity/Contact.java
+++ app/src/main/java/com/example/contactappsqlite/db/entity/Contact.java
@@ -1,21 +1,31 @@
 package com.example.contactappsqlite.db.entity;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "contacts")
 public class Contact {
 
     // database constants
-    public static final String TABLE_NAME = "contacts";
+    /*public static final String TABLE_NAME = "contacts";
     public static final String COLUMN_ID = "contact_id";
     public static final String COLUMN_NAME = "contact_name";
-    public static final String COLUMN_EMAIL = "contact_email";
+    public static final String COLUMN_EMAIL = "contact_email";*/
 
     // variables
+    @ColumnInfo(name = "contact_name")
     private String name;
+    @ColumnInfo(name = "contact_email")
     private String email;
+    @ColumnInfo(name = "contact_id")
+    @PrimaryKey(autoGenerate = true)
     private int id;
 
     // Constructor
-
     // 1 - empty
+    @Ignore
     public Contact() {}
 
     // 2 - parameterized
@@ -26,8 +36,6 @@ public class Contact {
     }
 
     // getter and setters
-
-
     public String getName() {
         return name;
     }
@@ -53,9 +61,9 @@ public class Contact {
     }
 
     // SQL Query: create table
-    public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "( "
+    /*public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "( "
             + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
             + COLUMN_NAME + " TEXT,"
             + COLUMN_EMAIL + " DATETIME DEFAULT CURRENT_TIMESTAMP"
-            + ")";
+            + ")";*/
 }
